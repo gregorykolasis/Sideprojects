544678773fab24dbdab7db7aa690dc8c94966c02
Kolasis <57864112+gregorykolasis@users.noreply.github.com>
2024-05-14 10:55:51 +0300

    Backend Deployer / monitorStartup
    Backend Deployer / monitorStartup

--- AF_Deployer_SSH/deployer.py
+++ AF_Deployer_SSH/deployer.py
@@ -3,8 +3,19 @@ import fabric
 import glob
 from modules.pysftp import CnOpts,Connection 
 import json
+from invoke.watchers import StreamWatcher
 
-global host
+class OutputWatcher(StreamWatcher):
+    def submit(self, stream):
+        print(f'Output: "{stream}"')
+        return []
+
+global host,room
+
+SOFTWARE   = 'Software'
+PROGRAMMER = 'Programmer'
+ANYDESK    = 'Anydesk'
+
 '''
 host = "192.168.1.29"
 username = "vasil"
@@ -34,11 +45,20 @@ def getAllFiles(folderPath):
 
 def executeTerminalCommand(cmd):
     global host
-    with fabric.Connection( host = host, port = port, user = username, connect_kwargs={"password": password} ) as c:
-        try:
-            c.run(cmd)                  
-        except Exception as e:
-            print(e)
+    c = fabric.Connection ( host = host, port = port, user = username, connect_kwargs={"password": password} , 
+        # config=fabric.Config(
+        # {
+        #     'run': {
+        #     'watchers': [
+        #         OutputWatcher(),
+        #     ],
+        #     },
+        # }) 
+    )
+    try:
+        c.run(cmd)
+    except Exception as e:
+        print(e)
 
 def createFolder(url):
     try:
@@ -53,49 +73,93 @@ def createFolder(url):
     except Exception as e:
         print(e)
 
-def deploy():
-    global host
+def deploy(folder,exludeFiles=None):
+    global host,room
     cnopts = CnOpts()
     cnopts.hostkeys = None
-    print(f"[deploy]Host:{host}")
-    with Connection(host, username=username, password = password , cnopts=cnopts) as sftp:
-        try:
-            #srcFolder = '''C:/Users/PROGregory/Documents/GitHub/AF_Controller/AF_Main_Controller''' #f'{Cur}/Mirror/{folder}'
-            #destFolder = f'{Home}IE/{"AF_Main_Controller"}'
-            folder = Folders[0]
-            srcFolder = f'{Cur}/Mirror/{folder}'
-            destFolder = f'{Home}IE/{folder}'
-            sftp.put_r(srcFolder,destFolder,excludeFiles=['config.ini'])
-        except Exception as IOError:
-            if IOError.args[0] == 2: #No such File
-                createFolder(destFolder)
+    #print(f"[deploy]Host:{host}")
+    try:
+        with Connection(host, username=username, password = password , cnopts=cnopts) as sftp:
+            try:
+                #srcFolder = '''C:/Users/PROGregory/Documents/GitHub/AF_Controller/AF_Main_Controller''' #f'{Cur}/Mirror/{folder}'
+                #destFolder = f'{Home}IE/{"AF_Main_Controller"}'
+                #folder = Folders[0]
+                srcFolder = f'{Cur}/Mirror/{folder}'
+                destFolder = f'{Home}IE/{folder}'
                 sftp.put_r(srcFolder,destFolder,excludeFiles=['config.ini'])
+            except Exception as IOError:
+                if IOError.args[0] == 2: #No such File
+                    createFolder(destFolder)
+                    sftp.put_r(srcFolder,destFolder,excludeFiles=['config.ini'])
+        print(f"[Deploy] Succeed")
+    except Exception as e:
+        print(f"[Deploy] General Error:{e}")
 
-def restartHost():
+def restartHost(): 
     cmd = 'shutdown /r /t 0'
     executeTerminalCommand(cmd)
+
+def shutdownHost():
+    cmd = 'shutdown /s /t 0'
+    executeTerminalCommand(cmd)
+
+def runConfig():
+    cmd = 'cd c:/IE/Software & python config.py'
+    executeTerminalCommand(cmd)
+
+def sayHello():
+    global room
+    cmd = 'echo Hi'
+    executeTerminalCommand(cmd)
+    print(f"[sayHello] Room:{room}")
+
+def getAnydesk():
+    cmd = 'cd c:/IE/Software/anydesk && getAnydesk.bat'
+    executeTerminalCommand(cmd)
+
+def deleteLogs():
+    cmd = 'cd c:/IE/Software/logs & del /f /q /S "*"'
+    executeTerminalCommand(cmd)
+
+def stopSoftware():
+    cmd = "taskkill /IM python.exe /F & taskkill /F /IM chrome.exe /T > nul & del /S c:/IE/Software/logs/"
+    executeTerminalCommand(cmd)
+
+def startSoftware(): #Not working
+    cmd = '''start "" "C:/Windows/system32/cmd.exe" "C:/Users/Mindtrap/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/startup.bat" '''
+    executeTerminalCommand(cmd)
+
+def clearLogs():
+    stopSoftware()
+    deleteLogs()
+    #startSoftware()
 
 FolderToGrab = 'Mirror'
 Home = '../../' #Going to C:/ in Windows
 Cur = getCurrentPath()
 Folders = getDeployFolders(f"{Cur}/{FolderToGrab}")
-Files = getAllFiles(f"{Cur}/{FolderToGrab}")
+
+#Files = getAllFiles(f"{Cur}/{FolderToGrab}")
 
 f = open(f'{Cur}/hostnames.json')
 Hostnames = json.load(f)
 
-def deployMirror():
-    global host
+canCheatRooms = ['justdoit','climbing','pharaoh','rabbithole','jungler','thefactory' , 'bubblebobble' ]
+
+def massJoblist():
+    global host,room
     for x in Hostnames:
         room = x
         ip = Hostnames[x]
-        if room=='justdoit':
-            print(f"[Mirorring]For:{room} at IP:{ip}")
-            host = ip
-            try:
-                deploy()
+        host = ip
+        try:
+            if True:
+            #print(f"[Deploying] For->{room}")
+                #deploy(SOFTWARE)
+                #runConfig()   
+                #clearLogs()
                 restartHost()
-            except Exception as e:
-                print(f"[deploy] Error:{e}")
+        except Exception as e:
+            print(f"[massJoblist] Error:{e} IP:{host} Room:{room}")
 
-deployMirror()
+massJoblist()
---
+++ MonitorOpenedProgramsStartup/monitorStartup.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import subprocess
+import time
+try:
+    import wmi
+except ModuleNotFoundError:
+    os.system("pip install wmi")
+    os.execl(sys.executable, os.path.abspath(__file__), *sys.argv) 
+
+# PYTHON_PATH         = "python"                     #r"C:\Users\intelligent69\AppData\Local\Programs\Python\Python39\python.exe"
+# CHECKER_SCRIPT_PATH = f"{path}/startupPrograms.py" #C:\Users\intelligent69\Desktop\OpenStartUpPrograms\checkStartupPrograms.py"
+programsNotOpened = []
+path = os.path.abspath(os.path.join(__file__, "../"))
+
+#--------------------------------------------CONFIGURATION----------------------------------------------
+SECONDS_SLEEP  = 30
+IDLE_TIME      = 5                                   #Seconds to wait for Processor CPU Usage < IDLE_PERCENT , meaning the PC completed startup
+IDLE_PERCENT   = 30                                  #Proccessor CPU Usage 
+startUpPrograms = ['laragon.exe','AnyDesk.exe']
+#--------------------------------------------CONFIGURATION----------------------------------------------
+
+def checkProcessRunning(ProgramName):
+    if(wmi.WMI().Win32_Process(name=ProgramName)):
+        print(ProgramName + " is running")
+    else:
+        print(ProgramName + " is not running")
+        programsNotOpened.append(ProgramName)
+
+def findFileLocation(ProgramName):
+    for root, dirs, files in os.walk('C:\\'):
+        if ProgramName in files:
+            return root
+    return None
+
+def startPrograms(ProgramName):
+    file_location = findFileLocation(ProgramName)
+    if file_location:
+        print(f"Found {ProgramName} at the following location:")
+        print(file_location)
+        program_path = os.path.join(file_location, ProgramName)
+        os.startfile(program_path)
+    else:
+        print(f"Could not find {ProgramName}")
+
+#============================================================================================================================
+
+def get_load():
+    output = subprocess.check_output('wmic cpu get loadpercentage', shell=True)
+    load = output.split()[1]
+    return int(load)
+
+def openPrograms():
+    #subprocess.call([PYTHON_PATH,CHECKER_SCRIPT_PATH])
+    print("Checking...")
+    for x in startUpPrograms:
+        checkProcessRunning(x)
+    for z in programsNotOpened:
+        startPrograms(z)
+
+def waitTillReady():
+    idleSeconds = 0
+    while idleSeconds < IDLE_TIME:
+        load = get_load()
+        print(f"CPU Usage:{load}% | {IDLE_TIME-idleSeconds} seconds until openPrograms")
+        if load < IDLE_PERCENT:
+            idleSeconds += 1
+        else:
+            idleSeconds = 0
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print(f"[monitorStartup] Waiting for {SECONDS_SLEEP} seconds before running...")
+    time.sleep(SECONDS_SLEEP)
+    waitTillReady()
+    try:
+        openPrograms()
+    except Exception as e:
+        print(f"openPrograms() Failed with Error:{e}")
